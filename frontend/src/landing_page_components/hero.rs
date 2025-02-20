9404e642626bb3d07bafaff1d4ad56eabd982324
Kubabob <j.m.m.bozek@gmail.com>
2025-02-20 14:56:10 +0100

    changed landing page hero img

--- frontend/src/landing_page_components/hero.rs
+++ frontend/src/landing_page_components/hero.rs
@@ -2,11 +2,10 @@ use yew::prelude::*;
 // use super::submissions_count::SubmissionCount;
 #[function_component(Hero)]
 pub fn hero() -> Html {
-
     html! {
         <section class="hero-section">
             <div class="image-container">
-                <img src="img/homepage-bg.jpg" alt="Super cool zdjęcie, które się jeszcze nie załadowało :)" class="faded-border-image" />
+                <img src="img/homepage-bg3.jpg" alt="Super cool zdjęcie, które się jeszcze nie załadowało :)" class="faded-border-image" />
             </div>
 
             <div class="hero-content">
@@ -14,7 +13,7 @@ pub fn hero() -> Html {
                     <span class="left-part">{"GRA"}</span>
                     <span class="right-part">{"na ORIENTACJE"}</span>
                 </h1>
-                
+
                 // <div class="stats-container">
                 //     <div class="stat-item">
                 //         <div class="stat-number">
@@ -24,12 +23,12 @@ pub fn hero() -> Html {
                 //     </div>
                 //     <div class="stat-item">
                 //         <div class="stat-number">
-                //             <SubmissionCount user_status="LGBT" />    
+                //             <SubmissionCount user_status="LGBT" />
                 //         </div>
                 //         <div class="stat-label">{"LGBT"}</div>
                 //     </div>
                 // </div>
-            </div>    
+            </div>
         </section>
     }
 }
