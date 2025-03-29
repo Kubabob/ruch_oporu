e937c471c51e7145e873c845a101128640f22c71
Kubabob <j.m.m.bozek@gmail.com>
2025-03-29 15:48:43 +0100

    deploying deploy

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -5,12 +5,16 @@ use frontend::components::navbar::Navbar;
 use frontend::components::routes::{Route, switch};
 use frontend::components::footer::Footer;
 
+fn base_path() -> String {
+    option_env!("PUBLIC_URL").unwrap_or("/").to_string()
+}
+
 #[function_component(App)]
 fn app() -> Html {
     html! {
         <>
             <Navbar />
-            <BrowserRouter>
+            <BrowserRouter basename={base_path()}>
                 <Switch<Route> render={switch} />
             </BrowserRouter>    
             <Footer />
